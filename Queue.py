cbc059a769b16b4a2777dff2bd079c16d9d702d8
Christian <christian2tv@gmail.com>
2022-01-21 19:55:59 +0100

    Update Queue.py

--- Queue.py
+++ Queue.py
@@ -6,12 +6,14 @@ class Queue:
     def __init__(self, data=None):
         self.__data = data or []
         self.__front = 0
+        
     def append(self, value):
         #Enqueue method
         if self.isEmpty():
             self.__data = []
             self.__front = 0
         self.__data.append(value)
+        
     def pop(self):
         #Dequeue method
         if self.isEmpty():
@@ -20,14 +22,18 @@ class Queue:
         self.__data[self.__front] = None
         self.__front += 1
         return value
+        
     def isEmpty(self):
         return self.__front == len(self.__data)
+        
     def __len__(self):
         return len(self.__data) - self.__front
+        
     def __str__(self):
-        return ', '.join(self.__data[:self.__front-len(self.__data)-1:-1])
+        return str(self.__data[:self.__front-len(self.__data)-1:-1])
+        
     def __repr__(self):
-        return self.__str__()       
+        return self.__str__()    
     
 '''if __name__ == "__main__":
     q = Queue(["a", "b", "c"])
